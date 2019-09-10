4d4b3ba264ec43fc87cabac7ca1e076d6f652ee8
darshan <darshankhot@protonmail.com>
2019-09-10 20:17:15 +0530

    nav

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,19 +1,21 @@
-<?php require_once '/studycampus/auth/checkLogin.php'; ?>
+<?php
+	// require_once '/studycampus/auth/checkLogin.php';
+?>
 <nav class="navbar navbar-light bg-light">
 	<a class="navbar-brand" href="/studycampus">StudyCampus</a>
 	<div>
-		<?php if (isLoggedIn()) { ?>
+		<?php /*if (isLoggedIn()) {*/ ?>
 		<a href="/studycampus/auth/login.php">Login</a>&nbsp;&nbsp;&nbsp;&nbsp;
 		<a href="/studycampus/auth/signup.php">Sign up</a>
 		<?php
-			}
-			else {
+			// }
+			// else {
 		?>
-			<a href="/studycampus/auth/login.php"><?= $_SESSION['name'] ?>'s account</a>&nbsp;&nbsp;&nbsp;&nbsp;
-			<a href="/studycampus/auth/logout.php">Logout</a>
+			<!-- <a href="/studycampus/auth/login.php"><?= $_SESSION['name'] ?>'s account</a>&nbsp;&nbsp;&nbsp;&nbsp; -->
+			<!-- <a href="/studycampus/auth/logout.php">Logout</a> -->
 
 		<?php
-			}
+			// }
 		?>
 	</div>
 </nav>
