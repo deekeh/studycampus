e8d07115b3538f5dc6e8324394cd6beeeea075bd
darshan <darshankhot@protonmail.com>
2019-09-11 22:04:23 +0530

    added dashboard sample page data

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,7 @@
+<?php
+    if (!isLogIn()) header('Location: auth/login.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +10,34 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script>
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        });
+    </script>
 </head>
 <body>
-    
+    <?php require_once 'layouts/navbar.php' ?>
+    <div class="container-fluid">
+        <h2>
+            <ol class="breadcrumb" style="background: #ffffff;">
+                <li class="breadcrumb-item">Dashboard</li>
+                <li class="breadcrumb-item"><?= $_SESSION['name'] ?></li>
+            </ol>
+        </h2>
+        <hr>
+
+        <div class="container">
+            <h4>
+                Courses enrolled
+            </h4>
+            <div class="list-group">
+                <a href="resource.php?c_id=1" data-toggle="tooltip" title="View resources" data-placement="left" class="list-group-item list-group-item-active">Course 1</a>
+                <a href="resource.php?c_id=1"  data-toggle="tooltip" title="View resources" data-placement="left" class="list-group-item list-group-item-active">Course 1</a>
+            </div>
+        </div>
+    </div>
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
