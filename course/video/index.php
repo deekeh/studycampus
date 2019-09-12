ff040b48e43142f56388aefda289420e76d75f2b
darshan <darshankhot@protonmail.com>
2019-09-12 21:33:54 +0530

    implementation of capturing and saving of video pause-time in console

--- course/video/index.php
+++ course/video/index.php
@@ -16,7 +16,6 @@
 		$v_url = $vid_detail['url'];
 		$v_id = $_GET['vid'];
 	}
-
 ?>
 
 <!DOCTYPE html>
@@ -26,23 +25,30 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="//cdn.jsdelivr.net/npm/afterglowplayer@1.1"></script>
-	<title>Document</title>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(function() {
+			$('#videox').on('pause', function(e) {
+				var videoHandler = $("#videox")[0];
+				console.log("Duration At Pause "+videoHandler.currentTime);
+			});
+		});
+	</script>
+
+	<title>StudyCampus | Watch</title>
 </head>
 <body>
 	<?php require_once '../../layouts/navbar.php' ?>
 	<div class="container">
 		<div class="row">
 		<div class="col-11 mt-3 mx-auto" >
-			<!-- <video controls class="afterglow" src="<?= '../../data/'. $v_url ?>" width="100%"></video> -->
-			<video autoplay class="afterglow" id="" width="1280" height="720" data-skin="dark">
+			<video controls autoplay id="videox" width="100%">
  				<source type="video/mp4" src="<?= '../../data/'. $v_url ?>" />
 			</video>
 		</div>
 		</div>
 	</div>
-
-
 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
